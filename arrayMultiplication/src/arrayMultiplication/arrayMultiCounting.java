da9e17fd3564a0b76a1941e9216abe945e2ce92e
onotoleussr <onotole.ussr@gmail.com>
2013-07-17 23:54:21 +0600

    Законченное задание по перемножению матриц

--- arrayMultiplication/src/arrayMultiplication/arrayMultiCounting.java
+++ arrayMultiplication/src/arrayMultiplication/arrayMultiCounting.java
@@ -1,10 +1,79 @@
 package arrayMultiplication;
 
+import java.io.IOException;
+
 public class arrayMultiCounting {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		System.out.println("Hello!!!");
+		
+		int[][] firstMatrix  = new int[3][3];
+		int[][] secondMatrix = new int[3][3];
+		
+		
+		System.out.println("Не будем выпендриваться, возьмем 2 квадратных матрицы размером 3\n");
+		System.out.println("Забиваем значениями по Random()*100");
+		
+		
+		//Заполняем первую и вторую матрицу сразу
+		for(int i=0; i<firstMatrix.length; i++){
+			
+			for(int j=0; j<secondMatrix.length; j++){
+			
+				firstMatrix[i][j]=(int) (Math.random()*100);
+				secondMatrix[i][j]=(int) (Math.random()*100);
+			}
+		}
+		
+		System.out.println("Значения первой матрици");
+		
+		for(int i=0; i<3; i++){
+			
+			for(int j=0; j<3; j++){
+			
+				System.out.print(firstMatrix[i][j]+((j<2)?" | ":""));
+			}
+			System.out.print("\n");
+		}
+		
+		System.out.println("\nЗначения второй матрици");
+		
+		for(int i=0; i<3; i++){
+			
+			for(int j=0; j<3; j++){
+			
+				System.out.print(secondMatrix[i][j]+ ((j<2)?" | ":""));
+			}
+			System.out.print("\n");
+		}
+		
+		//Создаем массив результата
+
+		int[][] resultMatrix = new int[firstMatrix.length][secondMatrix.length];
+		
+		//Расчитываем
+		
+		for(int i=0;i<3;i++){
+			
+			for(int j=0;j<3;j++){
+				
+				resultMatrix[i][j] = resultMatrix[i][j] + firstMatrix[i][j] * secondMatrix[j][i];
+				
+			}
+		}
+		
+		System.out.println("\nЗначения результатирующей матрици");
+		
+		for(int i=0;i<3;i++){
+			
+			for(int j=0;j<3;j++){
+				
+				System.out.print(resultMatrix[i][j] + ((j<2)?" | ":""));
+				
+			}
+
+			System.out.print("\n");
+		
+		}
 	}
-
 }
