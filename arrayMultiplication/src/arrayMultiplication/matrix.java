c0e479153a6d3484c08fd622b62a3c392e762f31
onotoleussr <onotole.ussr@gmail.com>
2013-07-22 01:33:30 +0600

    Добавли вывод данных матрицы

--- arrayMultiplication/src/arrayMultiplication/matrix.java
+++ arrayMultiplication/src/arrayMultiplication/matrix.java
@@ -3,8 +3,8 @@ package arrayMultiplication;
 public class matrix {
 
 	private int[][] matrixObj;
-	
-	public void newMatrix(int rows, int cols){
+
+	public void newMatrix(int rows, int cols) {
 		matrixObj = new int[rows][cols];
 
 		for (int i = 0; i < rows; i++) {
@@ -13,11 +13,11 @@ public class matrix {
 			}
 		}
 	}
-	
+
 	public int getLength() {
 		return matrixObj.length;
 	}
-	
+
 	public int getHeight() {
 		return matrixObj[0].length;
 	}
@@ -25,8 +25,22 @@ public class matrix {
 	public int getValue(int i, int j) {
 		return matrixObj[i][j];
 	}
+
 	public void setValue(int i, int j, int newValue) {
 		matrixObj[i][j] = newValue;
 	}
-	
+
+	public void printMatrixValues() {
+
+		System.out.println("\nЗначения матрици");
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(matrixObj[i][j] + ((j < 2) ? " | " : ""));
+			}
+
+			System.out.print("\n");
+		}
+	}
+
 }
