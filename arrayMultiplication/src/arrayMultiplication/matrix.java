e38568939e0c565f042f13e6bf37aa96c90192c0
onotoleussr <onotole.ussr@gmail.com>
2013-07-22 00:21:50 +0600

    Добавляем процедуру инициализации матрици

--- arrayMultiplication/src/arrayMultiplication/matrix.java
+++ arrayMultiplication/src/arrayMultiplication/matrix.java
@@ -4,10 +4,21 @@ public class matrix {
 
 	private int[][] matrixObj;
 	
+	
+	public void newMatrix(int rows, int cols){
+	
+		matrixObj = new int[rows][cols];	
+		
+	}
+	
+	
+	
 	public int GetMatrixLength() {
-		
 		return matrixObj.length;
-		
+	}
+	
+	public int GetMatrixHeight() {
+		return matrixObj[0].length;
 	}
 	
 }
