51a069c279933a9c46744661f34508e3b7a9ce8c
onotoleussr <onotole.ussr@gmail.com>
2013-07-22 00:12:39 +0600

    Добавил класс matrix

--- arrayMultiplication/src/arrayMultiplication/arrayMultiCounting.java
+++ arrayMultiplication/src/arrayMultiplication/arrayMultiCounting.java
@@ -1,10 +1,8 @@
 package arrayMultiplication;
 
-import java.io.IOException;
-
 public class arrayMultiCounting {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 		
 		int[][] firstMatrix  = new int[3][3];
---
+++ arrayMultiplication/src/arrayMultiplication/matrix.java
@@ -0,0 +1,13 @@
+package arrayMultiplication;
+
+public class matrix {
+
+	private int[][] matrixObj;
+	
+	public int GetMatrixLength() {
+		
+		return matrixObj.length;
+		
+	}
+	
+}
