3928aabc8cb14293539d048fa04eb17783f0fa17
onotoleussr <onotole.ussr@gmail.com>
2013-07-22 01:21:57 +0600

    Добавил геттер и сеттер для класса Matrix и переименовал методы получения длины и высоты матрицы

--- arrayMultiplication/src/arrayMultiplication/matrix.java
+++ arrayMultiplication/src/arrayMultiplication/matrix.java
@@ -4,21 +4,23 @@ public class matrix {
 
 	private int[][] matrixObj;
 	
-	
 	public void newMatrix(int rows, int cols){
-	
 		matrixObj = new int[rows][cols];	
-		
 	}
 	
-	
-	
-	public int GetMatrixLength() {
+	public int getLength() {
 		return matrixObj.length;
 	}
 	
-	public int GetMatrixHeight() {
+	public int getHeight() {
 		return matrixObj[0].length;
+	}
+
+	public int getValue(int i, int j) {
+		return matrixObj[i][j];
+	}
+	public void setValue(int i, int j, int newValue) {
+		matrixObj[i][j] = newValue;
 	}
 	
 }
